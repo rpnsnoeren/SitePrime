aff571c10956af823a07551013eaee755ca54126
Remco Snoeren <rpn.snoeren@outlook.com>
2024-11-17 18:49:49 +0100

    database

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // je configuratie hier
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
